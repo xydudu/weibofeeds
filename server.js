c1f22d84a1b8d5f9b9b843d2753663f5bb59bc5f
xue <xue@xue-desktop.(none)>
2011-05-21 14:56:43 +0800

    test mongodb

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 var http = require('http');
- 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var Schema = mongoose.Schema , 
+    ObjectId = Schema.ObjectId;
+
+var BlogPost = new Schema({
+    author    : ObjectId, 
+    title     : String, 
+    body      : String, 
+    date      : Date
+});
+
+var myModel = mongoose.model('BlogPost');
+
+var instance = new myModel();
+
+myModel.title = 'hello';
+myModel.save(function (err) {
+  //
+});
+
+
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
   res.end("Hello world in my home computer\n");
